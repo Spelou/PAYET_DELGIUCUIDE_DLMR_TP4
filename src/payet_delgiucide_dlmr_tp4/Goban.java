733ca845fe4ee22c2ad666b0114ec0d90732dfe7
Spelou <qdelamot@eleves.ec-nantes.fr>
2015-12-08 16:40:55 +0100

    Ajout de la classe goban

---
+++ src/payet_delgiucide_dlmr_tp4/Goban.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package payet_delgiucide_dlmr_tp4;
+
+/**
+ *
+ * @author Quentin
+ */
+public class Goban {
+    
+    //attributs
+    String plateau[][]; //palteau de jeu
+    
+    public Goban(){
+        System.out.println("Bienvenue dans le jeu de go");
+        //boucle de vérification
+        boolean test= true;//variable de vérification
+        while(test){
+            
+        }
+        
+    }
+    
+    
+    
+}
