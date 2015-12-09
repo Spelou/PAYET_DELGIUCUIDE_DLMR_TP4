c51f023a5f8ce4522f51d5ccce0beef115bac7ae
Rominet13 <volphoenix@outlook.fr>
2015-12-09 17:12:52 +0100

    méthode fusionGroupe, calculLiberte finis (ajoutPierre déjà)

--- src/payet_delgiucide_dlmr_tp4/Groupe.java
+++ src/payet_delgiucide_dlmr_tp4/Groupe.java
@@ -12,12 +12,14 @@ import java.util.ArrayList;
  * @author Romain
  */
 public class Groupe {
+
     //attributs
+
     private int numGroupe;
     private int liberte; //commence à 0, somme des liberté de chaque pierre du groupe
     private int etat; // ??j'sais plus
     private String couleur; //idem que pour celle de pierre (N ou B)
-    private int nombrePierre; 
+    private int nombrePierre;
     private ArrayList<Pierre> listePierres;
 
     public Groupe(int numGroupe, int liberte, String couleur, Pierre premierePierre) {
@@ -26,33 +28,37 @@ public class Groupe {
         this.etat = 1;
         this.couleur = couleur;
         nombrePierre = 1;
-        
+
         listePierres = new ArrayList<Pierre>();
         listePierres.add(premierePierre);
     }
-    
-    public void ajouterPierre(Pierre pie){
+
+    public void ajouterPierre(Pierre pie) {
         listePierres.add(pie);
-        
     }
-    public void fusion(Groupe g){
+
+    public void fusion(Groupe g) {
         listePierres.addAll(g.listePierres);
-        
+        g = null;
     }
-    public void calculLiberte(){
-        
-        
+
+    public void calculLiberte() {
+        for (int i = 0; i < listePierres.size(); i++) {
+            liberte+=listePierres.get(i).getLiberte();
+        }
     }
-    public void changerEtat(){
-        
+
+    public void changerEtat() {
+
     }
-    public void afficherliste(){
-        for(int i=0;i<listePierres.size();i++) {
-         System.out.println(listePierres.get(i).getCouleur().toString());
-      }
-        
+
+    public void afficherliste() {
+        for (int i = 0; i < listePierres.size(); i++) {
+            System.out.println(listePierres.get(i).getCouleur().toString());
+        }
+
     }
-    
+
 //getteurs
     public int getNumGroupe() {
         return numGroupe;
@@ -77,7 +83,7 @@ public class Groupe {
     public ArrayList<Pierre> getListePierres() {
         return listePierres;
     }
-    
+
 //setteurs sans celui de couleur (car à priori une pierre ne change pas de couleur) et sans celui du tableau (inutile)
     public void setNumGroupe(int numGroupe) {
         this.numGroupe = numGroupe;
