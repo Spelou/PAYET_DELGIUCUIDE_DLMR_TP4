31319358b3ff3846faee66c09b56a3af9f760f60
Spelou <qdelamot@eleves.ec-nantes.fr>
2015-12-10 09:02:30 +0100

    méthode non suicide en cours de complétion, et méthode miseAjourDeg

--- src/payet_delgiucide_dlmr_tp4/Pierre.java
+++ src/payet_delgiucide_dlmr_tp4/Pierre.java
@@ -13,7 +13,7 @@ public class Pierre {
     //attributs
     private String couleur; // N,B,V pour Noir, Blanc, Vide
     private int etat;  // 0 mort, 1 prise, 2 en jeu
-    private int liberte; // nombre de degré de liberté de 0 à 3
+    private int liberte; // nombre de degré de liberté de 0 à 4
     private int numGroupe;
 //getteurs
     
