cb67379aada338398d3c4453a2d7a7c0b3dfc0d0
Rominet13 <romain.del-giudice@eleves.ec-nantes.fr>
2015-12-10 21:14:23 +0100

    1 ère version jouable (si modif dans goban)

--- src/payet_delgiucide_dlmr_tp4/PAYET_DELGIUCIDE_DLMR_TP4.java
+++ src/payet_delgiucide_dlmr_tp4/PAYET_DELGIUCIDE_DLMR_TP4.java
@@ -18,33 +18,7 @@ public class PAYET_DELGIUCIDE_DLMR_TP4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Bienvenue dans le jeu de go, création du Goban");
-        //boucle de vérification
-        boolean test = true;//variable de vérification
-        //scanner de récupération
-        Scanner myScan = new Scanner(System.in);
-        int taille = 0;
-        while (test) {
-            System.out.println("Entrer la taille du goban (9,16,19)"); // on demande les données à l'utilisateur
-            int choix = myScan.nextInt();
-            taille = choix;
-            if ((choix == 9) || (choix == 16) || (choix == 19)) {
-                test = false; // sortie de boucle
-            } else {
-                System.out.println("Vous n'avez pas donné une taille correcte.");
-            }
-        }
-        Goban gob = new Goban(taille);
-        gob.afficher();
-        gob.poserPierre(0, 0, "N");
-        gob.afficher();
-        gob.poserPierre(0, 0, "B");
-        gob.afficher();
-        gob.poserPierre(0, 1, "N");
-        gob.poserPierre(1, 0, "N");
-        gob.poserPierre(0, 0, "B");
-        gob.afficher();  
-    }
-
+        Partie laPartie = new Partie();
+        laPartie.CestPartie();
 }
